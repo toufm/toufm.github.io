fb7eae96cce3f2af5a3e719a749eb0e735e6c96b
Douglas Moura <70299743+toufm@users.noreply.github.com>
2021-01-13 19:18:19 -0500

    project v2

---
+++ PROJECT2/update_index.py
@@ -0,0 +1,54 @@
+web={
+    'header':'header.html',
+    'footer':'footer.html',
+    'content':''
+}
+
+def anyfunction(x:float,y:int):
+    """[Anything descriptive information]
+
+    Args:
+        x (float): [pass in any array, called x]
+        y (int): [pass in any array, called x]
+
+    Returns:
+        [float]: [returns the multiplication of x and Y]
+    """
+    return x+y
+
+
+
+def readfile(filename:str):
+    """ reads a file, takes in filename
+
+    Args:
+        filename (str): filepointer
+
+    Returns:
+        str: the html string
+    """
+    with open(filename, "r") as file_handle:
+        file_contents = file_handle.read()
+        return file_contents
+    
+def create_index(filename:str,filecontent:str):
+    with open (filename, "w") as file_handle:
+        file_handle.write(filecontent)
+
+import pandas as pd
+import random as r
+
+x=list(range(1,11))
+y=[r.randint(1,100) for i in x]
+data={
+    'x':x,
+    'y':y
+}
+
+df=pd.DataFrame(data)
+
+header_content=readfile(web['header'])
+footer_content=readfile(web['footer'])
+web['content']=df.to_html(index=False,table_id='sales')
+index_content=header_content+web['content']+footer_content
+create_index('index.html',index_content)
