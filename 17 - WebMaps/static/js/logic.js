fed2e26552e1f44cca24e135c540d39c3dfc0952
Douglas Moura <70299743+toufm@users.noreply.github.com>
2021-01-23 12:58:21 -0500

    update

--- 17 - WebMaps/static/js/logic.js
+++ 17 - WebMaps/static/js/logic.js
@@ -13,11 +13,12 @@ var lightmap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/
 
 // Store API query variables
 var baseURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-// var mag = "$where=created_date between'2018-01-01T00:00:00' and '2019-01-01T00:00:00'";
-// var depth = "&complaint_type=Rodent";
-// var loc = "&$limit=1000";
-// Assemble API query URL
-// var url = baseURL + mag + depth + loc;
+
+// Create a function to adjust marker size depending on mag values
+function getRadius(mag) {
+    return mag * 5
+}
+
 // Grab data with d3
 d3.json(baseURL, function(response) {
     // Create a new marker cluster group
@@ -44,44 +45,12 @@ d3.json(baseURL, function(response) {
         }
     }
    
-        // Loop through data
-        function getRadius(d) {
-            if (d < 1){
-                return "#800026"
-            }
-            else if (d < 2) {
-                return "BD0026"
-            }
-            else if (d < 3) {
-                return "#E31A1C"
-            }
-            else if (d<4){
-                return "#FC4E2A"
-            }
-            else if (d<5){
-                return "#FD8D3C"
-            }
-            else {
-                return "#FEB24C"
-            }
-        }
-
-// Function to Determine Size of Marker Based on the Magnitude of the Earthquake
-function markerSize(magnitude) {
-    if (magnitude === 0) {
-      return 1;
-    }
-    return magnitude * 3;
-}
-// Create a function to adjust marker size depending on mag values
-function getRadius(mag) {
-    return mag * 5
-}
-        
     L.geoJSON(response, {
         pointToLayer: function (feature, latlng) {
             return L.circleMarker(latlng);
         },
+        
+    
         onEachFeature: function (feature, layer) {
             layer.bindPopup(
                 "Magnitude : " 
@@ -108,4 +77,28 @@ function getRadius(mag) {
         
 
     }).addTo(baseMap);
+// Create legend - update this area
+var legend = L.control({position: "bottomright" });
+legend.onAdd = function(){
+    // Create div for Legend
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [-10, 10, 30, 50, 70, 90]
+        labels = [];
+        var colors = [
+            "#800026",
+            "#BD0026",
+            "#E31A1C",
+            "#FC4E2A",
+            "#FD8D3C",
+            "#FEB24C"
+          ];
+    // Loop through density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+          div.innerHTML +=
+              '<i style="background:' + colors[i] + '"></i> ' +
+              grades[i] + (grades[i +1 ] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+      }
+      return div;
+    };
+    legend.addTo(baseMap)
 })      
